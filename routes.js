efbf8b5d1d4a337d2333932bb7e6ca4b211f4c13
Guilherme da Costa <guilhermedc93@gmail.com>
2021-12-10 16:17:56 -0300

    LOGIN POST route completed

--- routes.js
+++ routes.js
@@ -1,7 +1,8 @@
 const routes = require('express').Router();
 
 const userController = require('./src/controllers/userController');
+const loginController = require('./src/controllers/loginController');
 
 routes.post('/users', userController.store);
-
+routes.post('/login', loginController.login);
 module.exports = routes;
